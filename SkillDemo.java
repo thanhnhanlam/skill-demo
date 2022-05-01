0dbc75349a1d68e901b1e7a3f376256085cfafca
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2022-05-01 16:28:30 -0700

    Update SkillDemo.java

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,10 +1,10 @@
 public class SkillDemo {
-    
+
     public static String trim(String s) {
-        if (s.charAt(0) == ' ') {
+        while (s.charAt(0) == ' ') {
             s = s.substring(1);
         }
-        if (s.charAt(s.length() - 1) == ' ') {
+        while (s.charAt(s.length() - 1) == ' ') {
             s = s.substring(0, s.length() - 1);
         }
         return s;
